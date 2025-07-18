0671df1a57a64542efbf72618a59674d83f5b243
mikis1111 <michelesaladino11@gmail.com>
2025-07-18 05:16:13 +0200

    Fix foreign key issue in production migration

--- db/migrate/20250621064211_change_book_id_in_borrowers.rb
+++ db/migrate/20250621064211_change_book_id_in_borrowers.rb
@@ -1,5 +1,9 @@
 class ChangeBookIdInBorrowers < ActiveRecord::Migration[7.1]
   def change
-    remove_reference :borrowers, :book, foreign_key: true, index: true
+    if foreign_key_exists?(:borrowers, :books)
+      remove_reference :borrowers, :book, foreign_key: true, index: true
+    else
+      say "No foreign key to remove from borrowers", true
+    end
   end
 end
